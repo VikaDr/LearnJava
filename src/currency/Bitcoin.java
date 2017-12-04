22805db4a85cb012dafce8329cc192762f1cd9fd
viktoriia.druchenko <viktoriia.druchenko@gmail.com>
2017-12-04 20:02:24 +0200

    ArrayTwoDim changes

--- src/currency/Bitcoin.java
+++ src/currency/Bitcoin.java
@@ -1,21 +1,12 @@
 package currency;
 
-public class Bitcoin extends Currency implements IsFreelyConver{
+public class Bitcoin extends Currency{
     public Bitcoin(double kursNBU, double rateBuy, double rateSell) {
         super(kursNBU, rateBuy, rateSell);
 
     }
     public double exchangeCurrencyToGrn(int bitcoin){
         System.out.println("Operacia zapreschena NBU");
-        return kursNBU;
+        return bitcoin;
     }
-
-    @Override
-    public boolean getFreelyConv() {
-        return false;
-    }
-
-
-
-
 }
